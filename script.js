d8954f4b389f48d94023d283f08f8a40f0c4a7ae
Jep <jacob.dyke30@gmail.com>
2023-12-13 01:22:01 -0600

    First UI Mockup

---
+++ script.js
@@ -0,0 +1,18 @@
+const outsideCircle = document.querySelector('.outside-circle');
+const circle = document.querySelector('.circle');
+const insideCircle = document.querySelector('.inside-circle-1');
+
+document.addEventListener('mousemove', (e) => {
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
+
+    const moveX = (mouseX / windowWidth) * 100 - 50;
+    const moveY = (mouseY / windowHeight) * 100 - 50;
+
+    outsideCircle.style.transform = `translate(${moveX * -0.25}px, ${moveY * -0.25}px)`
+    circle.style.transform = `translate(${moveX * - 0.26}px, ${moveY * -0.26}px)`
+    insideCircle.style.transform = `translate(${moveX * -0.27}px, ${moveY * -0.27}px)`;
+});
